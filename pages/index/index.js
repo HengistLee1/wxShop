74183e61ce7e9b72dd07deb5a53194d8c49f80c0
HengistLee1 <392862724@qq.com>
2018-07-04 21:36:45 +0800

    完成首页(轮播图、分类、电影推荐、商品列表)

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    image: [
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+    iconArray: [
+        {
+                "iconUrl": '../../image/icon-qiandao.png',
+                "iconText": '签到'
+            },
+            {
+                "iconUrl": '../../image/icon-fujin.png',
+                "iconText": '附近'
+            },
+            {
+                "iconUrl": '../../image/icon-zhanhui.png',
+                "iconText": '游展'
+            },
+            {
+                "iconUrl": '../../image/icon-fuli.png',
+                "iconText": '福利'
+            },
+            {
+                "iconUrl": '../../image/icon-muma.png',
+                "iconText": '玩乐'
+            },
+            {
+                "iconUrl": '../../image/icon-xingxing.png',
+                "iconText": '周边'
+            },
+            {
+                "iconUrl": '../../image/icon-tiyu.png',
+                "iconText": '体育'
+            },
+            {
+                "iconUrl": '../../image/icon-qinzi.png',
+                "iconText": '亲子'
+            }
+    ],
+    movieList: [
+        {
+            "movieTitle": "11月20日话剧《风声》",
+            "movieImage": "../../image/huaju.jpeg"
+        },
+        {
+            "movieTitle": "11月20日话剧《风声》2",
+            "movieImage": "../../image/huaju.jpeg"
+        },
+        {
+            "movieTitle": "11月20日话剧《风声》3",
+            "movieImage": "../../image/huaju.jpeg"
+        },
+    ],
+    goodsList: [
+        {
+            goodsId: 1,
+            goodsName: "木村耀司登山旅行大学生户外情侣双肩背包外带小背包",
+            goodsImage: "../../image/test.jpg",
+            goodsAddress: "广州",
+            goodsPrice: "1111.00"
+        },
+        {
+            goodsId: 1,
+            goodsName: "木村耀司登山旅行大学生户外情侣双肩背包外带小背包2",
+            goodsImage: "../../image/test.jpg",
+            goodsAddress: "广州",
+            goodsPrice: "1111.00"
+        },
+        {
+            goodsId: 1,
+            goodsName: "木村耀司登山旅行大学生户外情侣双肩背包外带小背包3",
+            goodsImage: "../../image/test.jpg",
+            goodsAddress: "广州",
+            goodsPrice: "1111.00"
+        }
+    ]
   },
 
   /**
